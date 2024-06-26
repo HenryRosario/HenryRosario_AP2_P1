2375277840c11970b48d6d9d88df40deca0e1bdd
RosarioHenry <henryRosario056@gmail.com>
2024-06-25 23:24:24 -0400

    Commit API POST

--- app/src/main/java/com/example/henryrosario_ap2_p1/data/remote/PersonaApi.kt
+++ app/src/main/java/com/example/henryrosario_ap2_p1/data/remote/PersonaApi.kt
@@ -1,12 +1,20 @@
 package com.example.henryrosario_ap2_p1.data.remote
 
 import com.example.henryrosario_ap2_p1.data.remote.dto.PersonaDto
-
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 
 interface PersonaApi {
     @GET("api/Personas")
     suspend fun getPersona(): List<PersonaDto>
 
+    @POST("api/Personas")
+    suspend fun createPersona(@Body persona: PersonaDto): PersonaDto
+
+    @DELETE("api/Personas/{id}")
+    suspend fun deletePersona(@Path("id") id: Int)
 }
